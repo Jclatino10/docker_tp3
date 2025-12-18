6bc2bc24657c956e28290d925a5c3b507723ce36
Jclatino10 <153731351+Jclatino10@users.noreply.github.com>
2025-12-18 13:48:13 +0100

    Update launch.sh

--- etape2/launch.sh
+++ etape2/launch.sh
@@ -1,6 +1,17 @@
+# Création du réseau Docker
 docker network create tp3net2
+
+# Suppression des anciens containers
 docker rm -f http script data
+
+# Lancement du container MariaDB avec initialisation SQL
 docker run -d --name data --network tp3net2 -e MARIADB_RANDOM_ROOT_PASSWORD=1 -v "$PWD/sql:/docker-entrypoint-initdb.d" mariadb:11
+
+# Construction de l’image PHP avec mysqli
 docker build -t tp3-php-mysqli ./script
+
+# Lancement du container PHP-FPM avec mysqli
 docker run -d --name script --network tp3net2 -v "$PWD/src:/app" tp3-php-mysqli
+
+# Lancement du container Nginx
 docker run -d --name http --network tp3net2 -p 8080:80 -v "$PWD/src:/app" -v "$PWD/config/default.conf:/etc/nginx/conf.d/default.conf:ro" nginx:1.29
