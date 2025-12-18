271cc6229ae8a643594cbc1f84870eddad79caf5
Jclatino10 <153731351+Jclatino10@users.noreply.github.com>
2025-12-18 13:46:53 +0100

    Update launch.sh

--- etape1/launch.sh
+++ etape1/launch.sh
@@ -1,4 +1,11 @@
+# Création du réseau Docker
 docker network create tp3net1
-docker rm -f http script
+
+# Suppression des anciens containers
+docker rm -f http script 
+
+# Lancement du container PHP-FPM
 docker run -d --name script --network tp3net1 -v "$PWD/src:/app" php:8.3-fpm
+
+# Lancement du container Nginx et exposition du port 8080
 docker run -d --name http --network tp3net1 -p 8080:80 -v "$PWD/src:/app" -v "$PWD/config/default.conf:/etc/nginx/conf.d/default.conf:ro" nginx:1.29
